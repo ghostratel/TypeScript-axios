5f373a61727823b8a5bd14e3da51732c62150d86
冯罡 <1353640486@qq.com>
2019-07-11 23:04:25 +0800

    add merge.ts

---
+++ src/helpers/merge.ts
@@ -0,0 +1,7 @@
+import { Defaults } from '../types/index'
+
+export function mergeConf(defaultConf: Defaults, userConf: Defaults): Defaults {
+  const config = Object.create(null)
+
+  return config
+}
